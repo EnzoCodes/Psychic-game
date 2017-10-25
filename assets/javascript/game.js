b7c182539c8b934419e1c075cac875eda70f9e0c
Your Name <enzocnop@gmail.com>
2017-10-24 23:11:56 -0700

    input store issues and reset on loss issues

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -11,7 +11,7 @@ var win = 0;
 // Variable to hold losses.
 var loss = 0;
 // Variable to hold guess count/chances.
-var i;
+var i = 9;
 // Comaprison Variables
 var randLetter;
 var userInput;
@@ -19,41 +19,36 @@ var userInput;
 // Alphabet length (26)
 var len = alphabet.length;
 
-// Function Start
-    document.onkeyup = function(event) {
-        var userInput = event.key;
+// Capture and hold Keypress
+var activePrint = document.getElementById("activeLetters");
+
+var activeLetters = [];
+
+    document.onkeyup = function(aevent) {
+        activeLetters = aevent.key;
+        activePrint.textContent = activeLetters;
+    }
+// Non of this works ^^^ 
+
+
+// Main Function
+    document.onkeyup = function(bevent) {
+        // Capture user input.
+        var userInput = bevent.key;
         // Computer Choice
         var randLetter = alphabet[Math.floor(Math.random() * len)];
-        //Each Guess, counter -1.
-        for (var i = 9; i > 0; i--) {
-            if (userInput != randLetter) {
-                continue;
-            } else if (userInput === randLetter) {
-                win++; //adds 1 to win counter.
-                return; // Hopefully resets.
-            } else if (i == 0) {
-                loss++;
-                return; //maybe return??
-            }
-        }
-        var game_active = "</p><p>Wins: " + win + "</p><p>Loss: " + loss + "<p>Guess: " + userInput + "</p><p>Guesses: " + i + "</p>";
 
+        if (userInput == randLetter) {
+            win += 1; // adds 1 to win counter if condition met.
+            i = 9; // Reset Chances
+        } else if (userInput != randLetter) {
+            i -= 1; //Subtracts an attempt if incorrect guess is entered.
+        } if (i == 0) {
+            loss += 1;
+            i = 9; // Reset Chances
+        }
+        console.log(randLetter);
+
+        var game_active = "<p>Wins: " + win + "</p><p>Loss: " + loss + "<p>Guess: " + userInput + "</p><p>Guesses: " + i + "</p>";
         document.getElementById("game_active").innerHTML = game_active;
     }
-
-
- // App randomly picks a letter, and the user has to guess which letter the app chose. Put the following text on your page:
-
- // Guess what letter I'm thinking of
-
- // Wins: (# of times the user has guessed the letter correctly)
-
- // Losses: (# of times the user has failed to guess the letter correctly after exhausting all guesses)
-
- // Guesses Left: (# of guesses left. This will update)
-
- // Your Guesses So Far: (the specific letters that the user typed. Display these until the user either wins or loses.)
-
- // When the player wins, increase the Wins counter and start the game over again (without refreshing the page).
-
-// When the player loses, increase the Losses counter and restart the game without a page refresh (just like when the user wins).
