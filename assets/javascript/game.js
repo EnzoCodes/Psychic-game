a101fdb7fa1790f3f1298eb269c33e41c59f39ef
Your Name <enzocnop@gmail.com>
2017-10-25 19:04:48 -0700

    ITS ALIVE

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -13,42 +13,39 @@ var loss = 0;
 // Variable to hold guess count/chances.
 var i = 9;
 // Comaprison Variables
-var randLetter;
 var userInput;
-
+var activePrint;
+var activeLetters = [];
 // Alphabet length (26)
 var len = alphabet.length;
-
-// Capture and hold Keypress
-var activePrint = document.getElementById("activeLetters");
-
-var activeLetters = [];
-
-    document.onkeyup = function(aevent) {
-        activeLetters = aevent.key;
-        activePrint.textContent = activeLetters;
-    }
-// Non of this works ^^^ 
+var randLetter = alphabet[Math.floor(Math.random() * len)];
 
 
 // Main Function
-    document.onkeyup = function(bevent) {
+    document.onkeyup = function(event) {
         // Capture user input.
-        var userInput = bevent.key;
-        // Computer Choice
-        var randLetter = alphabet[Math.floor(Math.random() * len)];
+        userInput = event.key;
+        //
+        console.log(randLetter);
+        //
+        activeLetters.push(userInput);
+        //
+        document.getElementById("activeLetters").innerHTML = activeLetters.join(" ");
 
-        if (userInput == randLetter) {
+        if (userInput === randLetter) {
             win += 1; // adds 1 to win counter if condition met.
-            i = 9; // Reset Chances
+            randLetter = alphabet[Math.floor(Math.random() * len)];
+            activeLetters = [];
+            // document.getElementById("game_active").innerHTML = game_active;
+            i = 9;
+            // Reset Chances
         } else if (userInput != randLetter) {
             i -= 1; //Subtracts an attempt if incorrect guess is entered.
-        } if (i == 0) {
+        } if (i === 0) {
             loss += 1;
             i = 9; // Reset Chances
+            activeLetters = [];
         }
-        console.log(randLetter);
-
         var game_active = "<p>Wins: " + win + "</p><p>Loss: " + loss + "<p>Guess: " + userInput + "</p><p>Guesses: " + i + "</p>";
         document.getElementById("game_active").innerHTML = game_active;
     }
